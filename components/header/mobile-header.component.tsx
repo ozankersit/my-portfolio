811c0e77f2dd9de08c5cde6f6bc471ea8025adbd
ozankersit <ozankersit@gmail.com>
2024-10-30 11:54:04 +0300

    mobile header development

--- components/header/mobile-header.component.tsx
+++ components/header/mobile-header.component.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { MobileMenuIcon } from "@/icons/mobile-menu-icon.component";
+import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -12,43 +13,44 @@ const linkElements = [
 
 const MobileHeader = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
     <div>
       <button
         className="xl:hidden flex"
-        onClick={() => {
-          setIsNavOpen(!isNavOpen);
-        }}
+        onClick={() => setIsNavOpen(!isNavOpen)}
       >
         <MobileMenuIcon />
       </button>
-      <div
-        className={
-          isNavOpen
-            ? "block !fixed bg-blue left-0 top-0 w-[70%] max-w-xs h-screen overflow-auto z-[1000000]"
-            : "hidden"
-        }
-      >
-        <li className="list-none flex flex-col gap-3 cursor-pointer items-center mt-10">
-          {linkElements.map((item) => (
-            <Link key={item.id} href={item.route}>
-              <ul className="hover:text-blue text-white transition-colors duration-300">
-                {item.str}
-              </ul>
-            </Link>
-          ))}
-        </li>
-      </div>
-      <div
-        onClick={() => {
-          setIsNavOpen(!isNavOpen);
-        }}
-        className={
-          isNavOpen
-            ? "block opacity-80 !fixed bg-white left-0 top-0 z-[100000] w-screen h-screen"
-            : "hidden"
-        }
-      ></div>
+
+      <AnimatePresence>
+        {isNavOpen && (
+          <motion.div
+            initial={{ width: 0 }}
+            animate={{ width: "70%" }}
+            exit={{ width: 0 }}
+            transition={{ duration: 0.3 }}
+            className="!fixed bg-blue left-0 top-0 max-w-xs h-screen overflow-auto z-[1000000]"
+          >
+            <li className="list-none flex flex-col gap-3 cursor-pointer items-center mt-10">
+              {linkElements.map((item) => (
+                <Link key={item.id} href={item.route}>
+                  <ul className="hover:text-blue text-white transition-colors duration-300">
+                    {item.str}
+                  </ul>
+                </Link>
+              ))}
+            </li>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      {isNavOpen && (
+        <div
+          onClick={() => setIsNavOpen(false)}
+          className="block opacity-80 !fixed bg-white left-0 top-0 z-[100000] w-screen h-screen"
+        ></div>
+      )}
     </div>
   );
 };
