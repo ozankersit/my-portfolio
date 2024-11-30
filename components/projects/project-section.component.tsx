ed68e234671ebe0659efb5df6defe978ad794d84
ozankersit <ozankersit@gmail.com>
2024-11-30 18:42:37 +0300

    grunge design added to projects section

--- components/projects/project-section.component.tsx
+++ components/projects/project-section.component.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FC } from "react";
 
 interface IProps {
@@ -12,14 +13,17 @@ const ProjectSection: FC<IProps> = ({
   projectIcon,
 }) => {
   return (
-    <a target="_blank" href={`${projectLink}`}>
-      <div className="flex gap-10 items-center xl:justify-between justify-center pb-10 bg-white p-5 rounded-xl transition duration-300 ease-in-out hover:scale-110 flex-wrap h-full">
+    <Link target="_blank" href={`${projectLink}`}>
+      <div className="flex gap-10 items-center xl:justify-between justify-center pb-10 bg-white p-5 rounded-xl transition duration-300 ease-in-out hover:scale-105 flex-wrap h-full group">
         <div className="mx-auto">{projectIcon}</div>
         <div className="mb-auto mt-5 mx-auto">
           <div className="flex flex-col gap-1">
             <div className="flex flex-col xl:justify-between justify-center text-center">
               <span className="font-medium text-lg underline">
-                Technologies:
+                <span className="relative inline-block">
+                  Technologies:
+                  <span className="absolute left-0 bottom-0 h-[1px] rounded-lg bg-black w-0 transition-all duration-300 group-hover:w-full"></span>
+                </span>
               </span>
             </div>
 
@@ -31,7 +35,7 @@ const ProjectSection: FC<IProps> = ({
           </div>
         </div>
       </div>
-    </a>
+    </Link>
   );
 };
 
--- components/projects/projects.component.tsx
+++ components/projects/projects.component.tsx
@@ -5,27 +5,26 @@ import TailwindIcon from "@/icons/tailwind-icon.component";
 import CssIcon from "@/icons/css-icon.component";
 import SassIcon from "@/icons/sass-icon.component";
 import AccountShopIcon from "@/icons/accountshop-icon.component";
-import OzanKersitIcon from "@/icons/ozankersit-icon.component";
-import QuizAppIcon from "@/icons/quiz-app-icon.component";
 import SocipawIcon from "@/icons/socipaw-icon.component";
 import VoluplexIcon from "@/icons/voluplex-icon.component";
+import GrungeIcon from "@/icons/grunge-icon.component";
 
 const accountShopTechs = [
-  { id: 1, icon:<NextjsIcon/> },
+  { id: 1, icon: <NextjsIcon /> },
   { id: 2, icon: <TypeScriptIcon /> },
   { id: 3, icon: <TailwindIcon /> },
   { id: 4, icon: <CssIcon /> },
 ];
 
 const portfolioTechs = [
-  { id: 1, icon:<NextjsIcon/> },
+  { id: 1, icon: <NextjsIcon /> },
   { id: 2, icon: <TypeScriptIcon /> },
   { id: 3, icon: <TailwindIcon /> },
   { id: 4, icon: <SassIcon /> },
 ];
 
 const quizAppTechs = [
-  { id: 1, icon:<NextjsIcon/> },
+  { id: 1, icon: <NextjsIcon /> },
   { id: 2, icon: <TypeScriptIcon /> },
   { id: 3, icon: <TailwindIcon /> },
 ];
@@ -37,28 +36,35 @@ const Projects = () => {
         <span className="text-4xl underline">My Projects</span>
         <span className="text-blue text-2xl mt-6">Just click to view!</span>
       </div>
-      <div className="grid lg:grid-rows-3 grid-cols-1 gap-5 items-stretch w-full">
-      <ProjectSection
-        projectIcon={<AccountShopIcon />}
-        projectLink="https://accountshop-v2.vercel.app/"
-        projectTechs={accountShopTechs.map((item) => (
-          <div key={item.id}>{item.icon}</div>
-        ))}
-      />
-      <ProjectSection
-        projectIcon={<SocipawIcon />}
-        projectLink="https://socipaw.vercel.app/tr"
-        projectTechs={portfolioTechs.map((item) => (
-          <div key={item.id}>{item.icon}</div>
-        ))}
-      />
-      <ProjectSection
-        projectIcon={<VoluplexIcon />}
-        projectLink="https://voluplex.com/"
-        projectTechs={quizAppTechs.map((item) => (
-          <div key={item.id}>{item.icon}</div>
-        ))}
-      />
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 items-stretch w-full">
+        <ProjectSection
+          projectIcon={<AccountShopIcon />}
+          projectLink="https://accountshop-v2.vercel.app/"
+          projectTechs={accountShopTechs.map((item) => (
+            <div key={item.id}>{item.icon}</div>
+          ))}
+        />
+        <ProjectSection
+          projectIcon={<SocipawIcon />}
+          projectLink="https://socipaw.vercel.app/tr"
+          projectTechs={portfolioTechs.map((item) => (
+            <div key={item.id}>{item.icon}</div>
+          ))}
+        />
+        <ProjectSection
+          projectIcon={<VoluplexIcon />}
+          projectLink="https://voluplex.com/"
+          projectTechs={quizAppTechs.map((item) => (
+            <div key={item.id}>{item.icon}</div>
+          ))}
+        />
+         <ProjectSection
+          projectIcon={<GrungeIcon />}
+          projectLink="https://www.grungedesignstudio.com/"
+          projectTechs={portfolioTechs.map((item) => (
+            <div key={item.id}>{item.icon}</div>
+          ))}
+        />
       </div>
     </div>
   );
